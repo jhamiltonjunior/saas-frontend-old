588fac660825396d3f239db18b6fd39fe08db43f
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-02-13 21:02:04 -0300

    se o usuario n estiver logado ele sera redirecionado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
 
-import { getCookie, getCookies } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 
 // ** Icons Imports
 import Poll from 'mdi-material-ui/Poll'
@@ -24,88 +24,89 @@ import WeeklyOverview from 'src/views/dashboard/WeeklyOverview'
 import DepositWithdraw from 'src/views/dashboard/DepositWithdraw'
 import SalesByCountries from 'src/views/dashboard/SalesByCountries'
 
-const Dashboard = () => {  
+const Dashboard = () => {
   return (
-      <ApexChartWrapper>
-        <Grid container spacing={6}>
-          <Grid item xs={12} md={4}>
-            <Trophy />
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <StatisticsCard />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <WeeklyOverview />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <TotalEarning />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Grid container spacing={6}>
-              <Grid item xs={6}>
-                <CardStatisticsVerticalComponent
-                  stats='$25.6k'
-                  icon={<Poll />}
-                  color='success'
-                  trendNumber='+42%'
-                  title='Total Profit'
-                  subtitle='Weekly Profit'
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <CardStatisticsVerticalComponent
-                  stats='$78'
-                  title='Refunds'
-                  trend='negative'
-                  color='secondary'
-                  trendNumber='-15%'
-                  subtitle='Past Month'
-                  icon={<CurrencyUsd />}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <CardStatisticsVerticalComponent
-                  stats='862'
-                  trend='negative'
-                  trendNumber='-18%'
-                  title='New Project'
-                  subtitle='Yearly Project'
-                  icon={<BriefcaseVariantOutline />}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <CardStatisticsVerticalComponent
-                  stats='15'
-                  color='warning'
-                  trend='negative'
-                  trendNumber='-18%'
-                  subtitle='Last Week'
-                  title='Sales Queries'
-                  icon={<HelpCircleOutline />}
-                />
-              </Grid>
+    <ApexChartWrapper>
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={4}>
+          <Trophy />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <StatisticsCard />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <WeeklyOverview />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <TotalEarning />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Grid container spacing={6}>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='$25.6k'
+                icon={<Poll />}
+                color='success'
+                trendNumber='+42%'
+                title='Total Profit'
+                subtitle='Weekly Profit'
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='$78'
+                title='Refunds'
+                trend='negative'
+                color='secondary'
+                trendNumber='-15%'
+                subtitle='Past Month'
+                icon={<CurrencyUsd />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='862'
+                trend='negative'
+                trendNumber='-18%'
+                title='New Project'
+                subtitle='Yearly Project'
+                icon={<BriefcaseVariantOutline />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='15'
+                color='warning'
+                trend='negative'
+                trendNumber='-18%'
+                subtitle='Last Week'
+                title='Sales Queries'
+                icon={<HelpCircleOutline />}
+              />
             </Grid>
           </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <SalesByCountries />
-          </Grid>
-          <Grid item xs={12} md={12} lg={8}>
-            <DepositWithdraw />
-          </Grid>
-          <Grid item xs={12}>
-            <Table />
-          </Grid>
         </Grid>
-      </ApexChartWrapper>
-    )
+        <Grid item xs={12} md={6} lg={4}>
+          <SalesByCountries />
+        </Grid>
+        <Grid item xs={12} md={12} lg={8}>
+          <DepositWithdraw />
+        </Grid>
+        <Grid item xs={12}>
+          <Table />
+        </Grid>
+      </Grid>
+    </ApexChartWrapper>
+  )
 }
 
-export async function getServerSideProps({ req }: any) {
- const token = getCookie('token', { req })
+export async function getServerSideProps({ req, res }: any) {
+  const token = getCookie('token', { req })
 
- if (typeof token === undefined) {
-  
- }
+  if (token === undefined) {
+    res.writeHead(301, { location: '/login' });
+    res.end()
+  }
 
   console.log(typeof token)
 
